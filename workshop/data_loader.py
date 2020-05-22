b451e44b1d14c9046f8ef6420cf769216759ab49
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-05-23 04:01:15 +0530

    prevent loading of duplicate data

--- workshop/data_loader.py
+++ workshop/data_loader.py
@@ -8,6 +8,8 @@ def load_data(file_path):
     with open(file_path) as f:
         reader = csv.DictReader(f)
         for row in reader:
-            movie = Movie(title=row["title"])
-            db.session.add(movie)
+            movie = Movie.query.filter(Movie.title == row["title"]).first()
+            if movie is None:
+                movie = Movie(title=row["title"])
+                db.session.add(movie)
         db.session.commit()
