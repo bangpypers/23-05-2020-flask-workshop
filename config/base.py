ae828272eb2638774ecedf708457410a6f9a18f6
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-05-23 02:50:25 +0530

    add logging related configs

--- config/base.py
+++ config/base.py
@@ -1,3 +1,5 @@
 
 SQLALCHEMY_DATABASE_URI="sqlite:///workshop.db"
 SQLALCHEMY_TRACK_MODIFICATIONS=False
+import logging
+LOG_LEVEL = logging.ERROR
--- config/dev.py
+++ config/dev.py
@@ -1,1 +1,4 @@
 DEBUG = True
+import logging
+LOG_LEVEL = logging.DEBUG
+LOGFILE = "instance/workshop.log"
