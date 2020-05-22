897a4c31e3d927c32b717e3bf40d79bb3e642050
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-05-23 03:28:18 +0530

    improve modularity so that it can read a filegiven the path

--- workshop/data_loader.py
+++ workshop/data_loader.py
@@ -2,10 +2,10 @@ from .models import db
 from .models.movies import Movie
 
 
-def load_data():
+def load_data(file_path):
     """Helper function to load data using flask shell"""
     import csv
-    with open("movies.csv") as f:
+    with open(file_path) as f:
         reader = csv.DictReader(f)
         for row in reader:
             movie = Movie(title=row["title"])
