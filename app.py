3539453611c8fb5e162868614118cc7e2a060fd6
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-05-23 00:39:50 +0530

    add user table

--- app.py
+++ app.py
@@ -133,4 +133,16 @@ class Movie(db.Model):
         return "<Movie: #{} - {}>".format(self.id_, self.title)
 
 
+class User(db.Model):
+    __tablename__ = "users"
+    id_ = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, name):
+        """Initializer"""
+        self.name = name
+
+    def __repr__(self):
+        return "<Movie: #{} - {}>".format(self.id_, self.name)
+
 app.run(debug=True)
