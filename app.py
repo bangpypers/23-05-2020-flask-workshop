ab040dbb9e99fc67b45db7b87e0ba859074dcc7b
Vinay Keerthi <ktvkvinaykeerthi@gmail.com>
2020-05-22 21:27:55 +0530

    add html to the index

--- app.py
+++ app.py
@@ -10,7 +10,18 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     """This will give you the 'home' page"""
-    return "Hello, World"
+    return """<html>
+    <head>
+    <title>flask application</title>
+    </head>
+    <body>
+    <h1>Hello, world!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    Vel, quaerat est! Nemo laudantium explicabo eligendi alias ea facilis
+    autem aspernatur accusamus architecto consectetur impedit, beatae
+    reprehenderit neque est ipsum dolor.
+    </p></body>
+    </html>"""
 
 
 app.run(debug=True)
